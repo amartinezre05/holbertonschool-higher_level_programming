004654df6e568e199b110061bba03d9d1fcb1895
amartinezre05 <almartinezre05@gmail.com>
2020-01-01 22:37:43 +0000

    [add] use of loops and conditionals

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for num1 in range(0, 9):
+    for num2 in range(1, 10):
+        if num1 >= num2:
+            continue
+        elif num1 < 8:
+            print("{:d}{:d}, ".format(num1, num2), end="")
+        else:
+            print("{:d}{:d}".format(num1, num2))
