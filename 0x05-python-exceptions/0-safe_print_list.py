e47268820de464acaab26d9d1585757cef4575ac
amartinezre05 <almartinezre05@gmail.com>
2020-01-09 04:35:24 +0000

    [fix] The syntaxis

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,8 +2,8 @@
 def safe_print_list(my_list=[], x=0):
     i = 0
     try:
-        for elem in my list[:x]:
-            print("{}".format(elem, end=''))
+        for elem in my_list[:x]:
+            print("{}".format(elem), end='')
             i += 1
     except IndexError:
         print()
