a43de072d5cb180973200166df825068c3d4efc8
amartinezre05 <almartinezre05@gmail.com>
2020-01-14 03:06:55 +0000

    [chg] The syntaxis of the 7's lines

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
+"""Unittest for max_integer([..])
 """
-Unittest for the function max_integer
-"""
-
-
 import unittest
-
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
