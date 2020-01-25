f6ef1d71a19aa87147f9cfe0ceda6ff409950fe8
amartinezre05 <almartinezre05@gmail.com>
2020-01-25 00:12:59 +0000

    [add] validate order of sentences

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,9 +6,9 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """ Class that inherits of Rectangle """
     def __init__(self, size):
-        super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
 
     def area(self):
         return self.__size ** 2
