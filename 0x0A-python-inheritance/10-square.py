30cb8b23157819748a7eb27cdab72d5c8719c13e
amartinezre05 <almartinezre05@gmail.com>
2020-01-25 00:05:52 +0000

    [add] the super function

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,8 +6,8 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """ Class that inherits of Rectangle """
     def __init__(self, size):
-        super().__init__()
-        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.integer_validator("size", size)
         self.__size = size
 
     def area(self):
