2b04749ee99f243db95cf15a028bb59769ec2bf6
amartinezre05 <almartinezre05@gmail.com>
2020-01-26 18:39:35 +0000

    [fix] the mode of open

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -3,5 +3,5 @@ import json
 
 
 def load_from_json_file(filename):
-    with open(filename, mode='w', encoding='utf-8') as f:
+    with open(filename, encoding='utf-8') as f:
         return json.load(f)
