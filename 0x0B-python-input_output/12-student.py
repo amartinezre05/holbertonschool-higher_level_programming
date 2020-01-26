ca7be4ec4b100a3b66bb30e3fa747cba4bbcded9
amartinezre05 <almartinezre05@gmail.com>
2020-01-26 19:20:42 +0000

    [add] retrieves a dictionary representation

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Class student"""
+
+
+class Student:
+    """a student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        a_dict = {}
+        for i in attrs:
+            try:
+                a_dict[i] = self.__dict__[i]
+            except:
+                pass
+        return a_dict
