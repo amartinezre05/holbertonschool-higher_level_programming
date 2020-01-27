d40a93c31af9120adc45a2636a506e414114fcf6
amartinezre05 <almartinezre05@gmail.com>
2020-01-27 02:14:39 +0000

    [fix] the syntax of the method str

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -22,7 +22,7 @@ class Square(Rectangle):
 
     def __str__(self):
         """ Magic method str to print """
-        return "[Rectangle] ({}) {}/{} - {}".format(
+        return "[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.width)
 
     def update(self, *args, **kwargs):
