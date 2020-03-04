c11b52bea624b14070364063ea6ba404086ce251
amartinezre05 <almartinezre05@gmail.com>
2020-03-04 01:40:01 +0000

    Create the database

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- creates the database hbtn_0d_usa
+-- creates the table states
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256), PRIMARY KEY (id));
