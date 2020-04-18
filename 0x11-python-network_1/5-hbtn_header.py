44b3ec18447b63e73f2ba230848b53d8f7461ed5
amartinezre05 <almartinezre05@gmail.com>
2020-04-18 12:38:50 -0500

    disypaly the value

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" request to the URL and displays the value of the variable X-Request-Id """
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    call = requests.get(argv[1])
+    head = call.headers.get('X-Request-Id')
