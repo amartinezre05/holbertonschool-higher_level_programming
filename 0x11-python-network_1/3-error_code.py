819d1ff38fec76d5c49bf6859d21715f14e8e053
amartinezre05 <almartinezre05@gmail.com>
2020-04-18 12:50:16 -0500

    display the body

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" displays the body of the response  """
+from sys import argv
+from urllib import error
+from urllib import request
+
+
+if __name__ == "__main__":
+    url = argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as resp:
+            print(resp.read().decode('utf-8'))
+    except error.HTTPError as err:
+        print("Error code: {}".format(err.code))
