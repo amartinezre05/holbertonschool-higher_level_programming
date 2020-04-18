53186744bd7220e99edddfa24fdb0d3b5d1f7d39
amartinezre05 <almartinezre05@gmail.com>
2020-04-18 13:15:44 -0500

    fix

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,5 +6,5 @@ import requests
 if __name__ == "__main__":
     call = requests.get('https://intranet.hbtn.io/status')
     print('Body response:')
-    print('\t- type: {}'.format(call.text))
+    print('\t- type: {}'.format(type(call.text)))
     print('\t- content: {}'.format(call.text))
