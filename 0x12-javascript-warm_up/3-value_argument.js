4b06cd90b6bfc0e6e5c0be35f8afba16c6a5ff84
amartinezre05 <almartinezre05@gmail.com>
2020-05-05 22:46:26 -0500

    use process.argv

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const myVar = process.argv[2];
-if (myVar != undefined) {
-  console.log(myVar);
-} else {
+if (myVar === undefined) {
   console.log('No argument');
+} else {
+  console.log(myVar);
 }
